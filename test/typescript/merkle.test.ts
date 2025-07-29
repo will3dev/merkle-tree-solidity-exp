f6dcc048760028633320c3f2476011142db0263d
Will <willwaltrip@gmail.com>
2025-07-29 10:00:56 -0400

    updates

--- test/typescript/merkle.test.ts
+++ test/typescript/merkle.test.ts
@@ -1,3 +1,19 @@
 import { ethers } from "ethers";
 import { expect } from "chai";
-import { MerkleTree__factory } from "../typechain-types";
+import { beforeEach } from "node:test";
+
+describe("MerkleTree", function () {
+
+    let merkleTree: any;
+    let provider: ethers.JsonRpcProvider;
+    let owner: any;
+
+    beforeEach(async function () {
+        provider = new ethers.JsonRpcProvider("http://localhost:8545");
+        owner = provider.getSigner();
+
+        const merkleTreeFactory = await ethers.getContractFactory("MerkleTree");
+        merkleTree = await merkleTreeFactory.deploy();
+    })
+
+})
